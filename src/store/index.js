a0ea7fd7fc0c2d55af6f88d6b92f7d88e69e16b5
Suyeon Jung <suyeonjungdev@gmail.com>
2023-02-08 11:06:52 +0900

    Initial Commit

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+// vue 는 공식적으로 'vuex' 라는 서비스를 import 채택
+
+export default createStore({
+  // 앱의 상태를 관리할 수 있음
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
