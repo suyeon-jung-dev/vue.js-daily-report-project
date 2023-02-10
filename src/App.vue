920c5f753beb7f3ca7818c9f602972f33905c3bd
Suyeon Jung <suyeonjungdev@gmail.com>
2023-02-10 10:50:51 +0900

    Part 7. 네비게이션. 기본 구조로 원복

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>    <!-- 라우터에서 지정하는 path 에 대해 컴포넌트를 지정한다.-->
+  <div id="app">
+
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss" src="./assets/css/style.scss">
